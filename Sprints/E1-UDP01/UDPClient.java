25653bc145611d19c5dcee9f0b934a674e083780
Vasco Lima <111665678+limz12@users.noreply.github.com>
2026-09-17 14:33:50 +0100

    Falta bloquear sessão até o utilizador enviar a correta

--- Sprints/E1-UDP01/UDPClient.java
+++ Sprints/E1-UDP01/UDPClient.java
@@ -12,9 +12,13 @@ public class UDPClient {
         try {
             aSocket = new DatagramSocket();
 
-            byte[] m = "vou enviar esta mensagem ao servidor".getBytes();
+            //timeout maximo para obter resposta do socket
+            //aSocket.setSoTimeout(2000);
+
             InetAddress aHost = InetAddress.getByName("localhost");
             int serverPort = 6789;
+            /* REMOVIDO PARA NAO TER PROBLEMAS COM O CHECK DA ORDEM DOS PACOTES
+            byte[] m = "vou enviar esta mensagem ao servidor".getBytes();
 
             DatagramPacket request = new DatagramPacket(m, m.length, aHost, serverPort);
 
@@ -29,31 +33,43 @@ public class UDPClient {
             //Resposta ao CA2: apenas com new String(reply.getData), ele converte o comprimento do buffer para string dai ter muitos caracteres que nao sao mensagem.
             //para resolver isso foi dado um offset de 0, para este buffer quero que respondas desde o 0 "primeira posicao" até ao final  "m.lenght"
             System.out.println("Reply: " + new String(reply.getData(),0,m.length));
+             */
 
             //ler do teclado 4.1
             Scanner sc = new Scanner(System.in);
-            String inputTeclado;
+            String inputTeclado ="";
             int ordemMensagem = 1;
             do {
                 System.out.print("> ");
-                inputTeclado = ordemMensagem+","+sc.nextLine();
+
+                String textoLido = sc.nextLine();
+
+                // Usamos 'continue' em vez de 'return' para o ciclo não fechar o programa e pedir novo input.
+                if (textoLido.isBlank()) {
+                    System.out.println("Não pode conter espaços brancos ou estar vazio.");
+                    continue;
+                }
+
+                // Monta a string final com a ordem (comentar embaixo e descomentar a seguinte para alterar a ordem dos pacotes manualmente)
+                //inputTeclado = ordemMensagem + "," + textoLido;
+                inputTeclado = textoLido;
 
                 // enviar linha para o servidor
                 byte mensagem[] = inputTeclado.getBytes();
-                DatagramPacket pacoteMensagem = new DatagramPacket(mensagem,mensagem.length, aHost, serverPort);
+                DatagramPacket pacoteMensagem = new DatagramPacket(mensagem, mensagem.length, aHost, serverPort);
                 aSocket.send(pacoteMensagem);
                 ordemMensagem++;
 
-                //receber a resposta do servidor
+                //receber a resposta do servidor (bloqueante ele fica a espera da resposta)
                 byte[] bufferResposta = new byte[1000];
                 DatagramPacket resposta = new DatagramPacket(bufferResposta, bufferResposta.length);
                 aSocket.receive(resposta);
-                System.out.println("Reply: " + new String(resposta.getData(),0,mensagem.length));
+
+                // Garante que lê exatamente o tamanho da resposta enviada pelo SERVIDO,
+                // evitando cortar a resposta se ela tiver um tamanho diferente da mensagem enviada.
+                System.out.println("Reply: " + new String(resposta.getData(), 0, resposta.getLength()));
 
             } while (!inputTeclado.contains("!q"));
-
-
-
 
         } catch (SocketException e) { System.out.println("Socket: " + e.getMessage());
         } catch (IOException e)     { System.out.println("IO: " + e.getMessage());
--- Sprints/E1-UDP01/UDPServer.java
+++ Sprints/E1-UDP01/UDPServer.java
@@ -1,5 +1,7 @@
+import javax.xml.crypto.Data;
 import java.net.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class UDPServer {
 
@@ -12,14 +14,53 @@ public class UDPServer {
 
             System.out.println("Socket a correr em localhost:"+6789);
 
+            int ordemAtual = 0;
+
             while (true) {
+                //RECEBER DADOS DO CLIENTE
                 DatagramPacket request = new DatagramPacket(buffer, buffer.length);
                 aSocket.receive(request);
 
-                DatagramPacket reply = new DatagramPacket(request.getData(),
-                        request.getLength(), request.getAddress(), request.getPort());
+                //VERIFICAR ORDENACAO -- (Movido int ordemAtual para fora do ciclo)
 
-                aSocket.send(reply);
+                String mensagemCliente = new String(request.getData(),0,request.getLength());
+
+                //IR BUSCAR A POS ATE A ,
+                int pos = mensagemCliente.indexOf(',');
+                if(pos == -1){
+                    String erro = "ERRO! A mensagem do cliente nao segue o padrao <N>,<mensagem>";
+                    System.out.println(erro);
+
+
+                    DatagramPacket respostaErro = new DatagramPacket(erro.getBytes(),erro.length(), request.getAddress(),request.getPort());
+                    aSocket.send(respostaErro);
+                    continue;
+                }
+
+                //LOGICA: SE O ATUAL EX = 1 FOR IGUAL AO ATUAL = 0 +1 QUER DIZER QUE E O CORRETO
+                String checkOrdem = String.valueOf(ordemAtual + 1 );
+                System.out.println("Valor do checkOrdem: "+checkOrdem);
+                System.out.println("Valor da ordem da mensagem do cliente: "+mensagemCliente.substring(0,pos));
+
+                if(mensagemCliente.substring(0,pos).equals(checkOrdem) && mensagemCliente != null){
+
+                    //RESPOSTA DO SERVIDOR (apenas com a nova mensagem)
+
+                    DatagramPacket respostaCliente = new DatagramPacket(mensagemCliente.getBytes(),
+                            request.getLength(), request.getAddress(), request.getPort());
+                    aSocket.send(respostaCliente);
+
+                    ordemAtual++;
+
+                } else {
+                    System.out.println("ERRO! A mensagem do cliente nao está ordenada ou então está vazia, à espera da ordem correta....");
+                    continue;
+
+                }
+
+
+                System.out.println("Recebi cliente: " +mensagemCliente);
+
             }
         } catch (SocketException e) { System.out.println("Socket: " + e.getMessage());
         } catch (IOException e)     { System.out.println("IO: " + e.getMessage());
